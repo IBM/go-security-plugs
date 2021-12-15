7f5d1af4a5c0f7e9711959b79333cd6da85660a2
David Hadas <david.hadas@gmail.com>
2021-12-15 12:04:20 +0200

    initialize

--- reverseproxyplugs/reverseproxyplugs_test.go
+++ reverseproxyplugs/reverseproxyplugs_test.go
@@ -1,6 +1,43 @@
 package reverseproxyplugs
 
-import "testing"
+import (
+	"net/http"
+	"testing"
+)
+
+type plug struct{}
+
+func (p plug) Initialize(l Logger) {
+	log = l
+	log.Infof("OK")
+}
+
+func (plug) Shutdown() {
+	log.Infof("OK")
+}
+
+func (plug) PlugName() string {
+	return "OK"
+}
+
+//ErrorHook(http.ResponseWriter, *http.Request, error)
+func (plug) ErrorHook(w http.ResponseWriter, req *http.Request, e error) {
+	log.Infof("OK")
+}
+
+//ResponseHook(*http.Response) error
+func (plug) ResponseHook(resp *http.Response) error {
+	log.Infof("OK")
+	return nil
+}
+
+//RequestHook(http.ResponseWriter, *http.Request) error
+func (plug) RequestHook(w http.ResponseWriter, r *http.Request) error {
+	log.Infof("OK")
+	return nil
+}
+
+var Plug plug
 
 func TestLoadPlugs(t *testing.T) {
 	var ext []string
@@ -12,14 +49,6 @@ func TestLoadPlugs(t *testing.T) {
 	ext = []string{}
 	if numTests = LoadPlugs(nil, ext); numTests != 0 {
 		t.Errorf("LoadPlugs expected 0 returned %d\n", numTests)
-	}
-	ext = []string{"../examplegate/examplegate.so"}
-	if numTests = LoadPlugs(nil, ext); numTests != 1 {
-		t.Errorf("LoadPlugs expected 1 returned %d\n", numTests)
-	}
-	ext = []string{"../examplegate/examplegate.so", "../examplegate/examplegate.so"}
-	if numTests = LoadPlugs(nil, ext); numTests != 2 {
-		t.Errorf("LoadPlugs expected 2 returned %d\n", numTests)
 	}
 }
 
