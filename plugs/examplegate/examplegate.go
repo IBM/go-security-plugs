6a1cb59ab6b490daac8416f4787876bad18c594c
David Hadas <david.hadas@gmail.com>
2021-12-15 15:32:35 +0200

    Update README.md
    Delete examplegate.so
    
    Update README.md
    
    remove package cycle
    
    Add files via upload
    
    Update README.md
    
    typos
    
    testing

--- plugs/examplegate/examplegate.go
+++ plugs/examplegate/examplegate.go
@@ -3,61 +3,57 @@ package main
 import (
 	"net/http"
 
-	"github.com/IBM/go-security-plugs/reverseproxyplugs"
+	"github.com/IBM/go-security-plugs/pluginterfaces"
 )
 
-const version string = "0.0.6"
+const version string = "0.0.7"
 
 type plug struct {
 	version string
+	log     pluginterfaces.Logger
 }
 
-var Plug plug
-var log reverseproxyplugs.Logger
+var Plug plug = plug{version: version}
 
-func init() {
-	Plug.version = version
+func (p plug) Initialize(l pluginterfaces.Logger) {
+	p.log = l
+	p.log.Infof("ExampleGate: Initializing - version %v\n", p.version)
 }
 
-func (p plug) Initialize(l reverseproxyplugs.Logger) {
-	log = l
-	log.Infof("ExampleGate: Initializing - version %v\n", p.version)
+func (p plug) Shutdown() {
+	p.log.Infof("ExampleGate: Shutdown")
 }
 
-func (plug) Shutdown() {
-	log.Infof("ExampleGate: Shutdown")
-}
-
-func (plug) PlugName() string {
+func (p plug) PlugName() string {
 	return "ExampleGate"
 }
 
 //ErrorHook(http.ResponseWriter, *http.Request, error)
-func (plug) ErrorHook(w http.ResponseWriter, req *http.Request, e error) {
-	log.Infof("ExampleGate: ErrorHook started")
+func (p plug) ErrorHook(w http.ResponseWriter, req *http.Request, e error) {
+	p.log.Infof("ExampleGate: ErrorHook started")
 }
 
 //ResponseHook(*http.Response) error
-func (plug) ResponseHook(resp *http.Response) error {
-	log.Infof("ExampleGate: ResponseHook started")
+func (p plug) ResponseHook(resp *http.Response) error {
+	p.log.Infof("ExampleGate: ResponseHook started")
 
 	for name, values := range resp.Header {
 		// Loop over all values for the name.
 		for _, value := range values {
-			log.Infof("ExampleGate Response Header: %s: %s", name, value)
+			p.log.Infof("ExampleGate Response Header: %s: %s", name, value)
 		}
 	}
 	return nil
 }
 
 //RequestHook(http.ResponseWriter, *http.Request) error
-func (plug) RequestHook(w http.ResponseWriter, r *http.Request) error {
-	log.Infof("ExampleGate: RequestHook started")
+func (p plug) RequestHook(w http.ResponseWriter, r *http.Request) error {
+	p.log.Infof("ExampleGate: RequestHook started")
 
 	for name, values := range r.Header {
 		// Loop over all values for the name.
 		for _, value := range values {
-			log.Infof("ExampleGate Request Header: %s: %s", name, value)
+			p.log.Infof("ExampleGate Request Header: %s: %s", name, value)
 		}
 	}
 	return nil
--- reverseproxyplugs/reverseproxyplugs_test.go
+++ reverseproxyplugs/reverseproxyplugs_test.go
@@ -5,9 +5,10 @@ import (
 	"testing"
 )
 
+/*
 type plug struct{}
 
-func (p plug) Initialize(l Logger) {
+func (p plug) Initialize(l pluginterfaces.Logger) {
 	log = l
 	log.Infof("OK")
 }
@@ -38,17 +39,44 @@ func (plug) RequestHook(w http.ResponseWriter, r *http.Request) error {
 }
 
 var Plug plug
+*/
+type countLog int
+
+func (countLog) Debugf(format string, args ...interface{}) {
+}
+func (countLog) Infof(format string, args ...interface{}) {
+}
+func (c countLog) Warnf(format string, args ...interface{}) {
+	c++
+}
+func (c countLog) Errorf(format string, args ...interface{}) {
+	c++
+}
 
 func TestLoadPlugs(t *testing.T) {
-	var ext map[string]interface{}
+	var config map[string]interface{}
 	var numTests int
-	if numTests = LoadPlugs(nil, nil); numTests != 0 {
+
+	if numTests = LoadPlugs(nil, "", nil); numTests != 0 {
 		t.Errorf("LoadPlugs expected 0 returned %d\n", numTests)
 	}
 
-	if numTests = LoadPlugs(nil, ext); numTests != 0 {
+	if numTests = LoadPlugs(nil, "", nil); numTests != 0 {
 		t.Errorf("LoadPlugs expected 0 returned %d\n", numTests)
 	}
+
+	if numTests = LoadPlugs(nil, "../plugs", nil); numTests != 1 {
+		t.Errorf("LoadPlugs expected 1 returned %d\n", numTests)
+	}
+
+	var log countLog
+	if numTests = LoadPlugs(log, "../plugs", config); numTests != 2 {
+		t.Errorf("LoadPlugs expected 2 returned %d\n", numTests)
+	}
+	if log > 0 {
+		t.Errorf("LoadPlugs expected 0 warnings and errors, received  %d\n", log)
+	}
+
 }
 
 //handleRequest
@@ -56,3 +84,25 @@ func TestLoadPlugs(t *testing.T) {
 //HandleResponsePlugs
 //HandleErrorPlugs
 //ShutdownPlugs
+
+func TestHandleRequestPlugs(t *testing.T) {
+	type args struct {
+		h http.Handler
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want http.Handler
+	}{
+		// TODO: Add test cases.
+		{"", args{nil}, nil},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := HandleRequestPlugs(tt.args.h); got != tt.want {
+				t.Errorf("HandleRequestPlugs() returned = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
